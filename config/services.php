fefd9e8e839133e5a1f4572d49c78e9fb0483aa3
Martin <hello@northrook.com>
2024-02-26 08:01:55 +0000

    mend

--- config/services.php
+++ config/services.php
@@ -4,13 +4,14 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 //use Northrook\Symfony\Latte\Services\EnvironmentService;
 
-function fromRoot( string $set = '' ) : string {
-	return '%kernel.project_dir%' . DIRECTORY_SEPARATOR . trim(
-			str_replace( [ '\\', '/' ], DIRECTORY_SEPARATOR, $set ), DIRECTORY_SEPARATOR,
-		) . DIRECTORY_SEPARATOR;
-}
 
 return static function ( ContainerConfigurator $container ) : void {
+
+	function fromRoot( string $set = '' ) : string {
+		return '%kernel.project_dir%' . DIRECTORY_SEPARATOR . trim(
+				str_replace( [ '\\', '/' ], DIRECTORY_SEPARATOR, $set ), DIRECTORY_SEPARATOR,
+			) . DIRECTORY_SEPARATOR;
+	}
 
 	$container->parameters()
 	          ->set( 'dir.templates', fromRoot( "/templates" ) )
