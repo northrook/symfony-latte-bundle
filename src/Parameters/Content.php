3e89ffdc23126776e7149825b632dac8d1515a5e
Martin <hello@northrook.com>
2024-03-21 20:45:19 +0000

    mend

--- src/Parameters/Content.php
+++ src/Parameters/Content.php
@@ -5,4 +5,20 @@ namespace Northrook\Symfony\Latte\Parameters;
 class Content
 {
 
+    private string $content = '';
+
+    public function __construct() {}
+
+    public function __toString() : string {
+        return $this->content;
+    }
+
+    public function getContent() : string {
+        return $this->content;
+    }
+
+    public function setContent( string $content ) : self {
+        $this->content = $content;
+        return $this;
+    }
 }
--- src/Parameters/Document.php
+++ src/Parameters/Document.php
@@ -2,10 +2,27 @@
 
 namespace Northrook\Symfony\Latte\Parameters;
 
+use Northrook\Elements\Body;
+
 class Document
 {
+
+    public readonly Body $body;
+
     public string $title = __METHOD__;
-    
+
+    public function __construct(
+        private readonly Application $application,
+        private readonly Content     $content,
+    ) {
+        $this->body = new Body(
+            id          : $this->application->request->getPathInfo(),
+            class       : 'test cass',
+            data_strlen : strlen( $this->content->__toString() ),
+        );
+    }
+
+
     public function __get( string $name ) {
         $name = "get" . ucfirst( $name );
         if ( method_exists( $this, $name ) ) {
@@ -18,4 +35,6 @@ class Document
     protected function getBase() : string {
         return __DIR__ . '/document.latte';
     }
+
+
 }
