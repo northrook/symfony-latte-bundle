d439c7ddc1176c381c7e88eb0ad58438783a1e17
Martin <hello@northrook.com>
2024-03-22 07:52:44 +0000

    mend

---
+++ src/Parameters/Content/Meta.php
@@ -0,0 +1,48 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Northrook\Symfony\Latte\Parameters\Content;
+
+use Northrook\Support\Str;
+
+/**
+ * @property string $name
+ * @property string $content
+ * @property bool   $printed
+ */
+final class Meta
+{
+    private string $name;
+    private string $content;
+    private bool   $printed = false;
+
+    public function __construct(
+        string                  $name,
+        string                  $content,
+        public readonly ?string $group = null,
+    ) {
+        $this->name    = Str::key( $name );
+        $this->content = Str::sanitize( $content );
+    }
+
+    public function __get( string $name ) {
+        if ( property_exists( $this, $name ) ) {
+            return $this->$name;
+        }
+        return null;
+    }
+
+    public function __toString() : string {
+        return "<meta name=\"$this->name\" content=\"$this->content\">";
+
+    }
+
+    public function get() : array {
+        $this->printed = true;
+        return [
+            $this->name => $this->content,
+        ];
+    }
+
+}
