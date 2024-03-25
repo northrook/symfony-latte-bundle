b40b3335ad453f5dce8bd7f1851263f6b7b0ad19
Martin <hello@northrook.com>
2024-03-25 12:13:22 +0000

    mend

--- src/Parameters/Document.php
+++ src/Parameters/Document.php
@@ -77,6 +77,19 @@ class Document
         $this->meta[ $meta->name ] = $meta;
     }
 
+    public function favicon() : array {
+        return [
+            '<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">',
+            '<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">',
+            '<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">',
+            '<link rel="manifest" href="/img/favicon/site.webmanifest">',
+            '<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#ff2d20">',
+            '<link rel="shortcut icon" href="/img/favicon/favicon.ico">',
+            '<meta name="msapplication-TileColor" content="#ff2d20">',
+            '<meta name="msapplication-config" content="/img/favicon/browserconfig.xml">',
+        ];
+    }
+
     public function meta( ...$get ) : array {
         $return = [];
 
---
+++ src/Parameters/Document/Favicon.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Northrook\Symfony\Latte\Parameters\Document;
+
+class Favicon
+{
+
+}
